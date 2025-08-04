49e6ca99b00d40eeba8d6b7936188d8cca81a2ea
k1berteftel <kkulis985@gmail.com>
2025-08-04 13:03:34 +0300

    update

--- utils/request_funcs.py
+++ utils/request_funcs.py
@@ -174,16 +174,12 @@ async def turn_off_job(cookies: str, jobs: list[Order], job_page=1) -> bool:
             cookies = json.load(f)
         await context.add_cookies(cookies)
 
+        counter = 0
         page = await context.new_page()
+        await page.goto(f'https://tmsmm.ru/social/orders?s=-1&page={job_page}')
         for i in range(0, len(jobs)):
             job_id = jobs[i].id
-            print(job_page)
-            try:
-                await page.goto(f'https://tmsmm.ru/social/orders?s=-1&page={job_page}')
-            except Exception:
-                await asyncio.sleep(5)
-                await page.goto(f'https://tmsmm.ru/social/orders?s=-1&page={job_page}')
-            await asyncio.sleep(1.5)
+            await asyncio.sleep(0.5)
             try:
                 await page.wait_for_selector(f'#bTaskT1Delete_{job_id}', timeout=4000.00)
                 await page.click(f'#bTaskT1Delete_{job_id}', button='left')
@@ -200,7 +196,7 @@ async def turn_off_job(cookies: str, jobs: list[Order], job_page=1) -> bool:
                     await page.click(f'#bTaskT1Cancel_{job_id}')
                 except Exception as err_2:
                     print('Удаление задачи 1.2 ', err_2)
-                    job_page += 1
+                    counter += 1
                 try:
                     await page.wait_for_selector(f'#bTaskT1Delete_{job_id}', timeout=4000.00)
                     await page.click(f'#bTaskT1Delete_{job_id}', button='left')
@@ -208,7 +204,12 @@ async def turn_off_job(cookies: str, jobs: list[Order], job_page=1) -> bool:
                     await page.wait_for_selector(f'#bTaskT1Delete_{job_id}', timeout=4000.00)
                     await page.click(f'#bTaskT1Delete_{job_id}', button='left')
                 except Exception:
+                    counter += 1
                     ...
+            if counter == 2:
+                page += 1
+                await page.goto(f'https://tmsmm.ru/social/orders?s=-1&page={job_page}')
+            counter = 0
             await asyncio.sleep(1)
         await context.close()
         await browser.close()
